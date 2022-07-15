4f944c5826868c92681d06f1498a23bb6040e955
geuno <rlfrmsdh1@gmail.com>
2022-07-15 18:45:01 +0900

    format: formatting 적용

--- realworld/src/main/java/org/realworld/demo/controller/dto/UserDto.java
+++ realworld/src/main/java/org/realworld/demo/controller/dto/UserDto.java
@@ -1,171 +1,181 @@
 package org.realworld.demo.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.realworld.demo.domain.user.entity.User;
 
 public class UserDto {
 
-    private UserDto(){}
+  private UserDto() {
+  }
 
-    public static class UserUpdateRequest{
-        @JsonProperty(value = "user")
-        public UserUpdateRequest.Request request;
+  public static class UserUpdateRequest {
 
-        public String getEmail(){
-            return request.email;
-        }
+    @JsonProperty(value = "user")
+    public UserUpdateRequest.Request request;
 
-        public String getUsername(){
-            return request.username;
-        }
-
-        public String getPassword(){
-            return request.password;
-        }
-
-        public String getImage(){
-            return request.image;
-        }
-
-        public String getBio(){
-            return request.bio;
-        }
-
-        public static class Request{
-            public final String email;
-
-            public final String username;
-
-            public final String password;
-
-            public final String image;
-
-            public final String bio;
-
-            public Request(String email, String username, String password, String image, String bio){
-                this.email = email;
-                this.username = username;
-                this.password = password;
-                this.image = image;
-                this.bio = bio;
-            }
-
-        }
+    public String getEmail() {
+      return request.email;
     }
 
-    public static class UserCreateRequest {
-
-        @JsonProperty(value = "user")
-        public UserCreateRequest.Request request;
-
-        public User toUser(){
-            return request.toUser();
-        }
-
-        public static class Request{
-
-            private final String username;
-
-            private final String email;
-
-            private final String password;
-
-            public Request(String username, String email, String password){
-                this.username = username;
-                this.email = email;
-                this.password = password;
-            }
-
-            public User toUser(){
-                return new User(email, password, username, "", "");
-            }
-        }
+    public String getUsername() {
+      return request.username;
     }
 
-    public static class UserResponse {
-
-        @JsonProperty(value="user")
-        public Response response;
-
-        public Response getResponse() {
-            return response;
-        }
-
-        public static class Response{
-            private final String email;
-
-            private final String token;
-
-            private final String username;
-
-            private final String bio;
-
-            private final String image;
-
-            private Response(String email, String token, String username, String bio, String image) {
-                this.email = email;
-                this.token = token;
-                this.username = username;
-                this.bio = bio;
-                this.image = image;
-            }
-
-            public String getEmail() {
-                return email;
-            }
-
-            public String getToken() {
-                return token;
-            }
-
-            public String getUsername() {
-                return username;
-            }
-
-            public String getBio() {
-                return bio;
-            }
-
-            public String getImage() {
-                return image;
-            }
-        }
-
-        public UserResponse(Response response){
-            this.response = response;
-        }
-
-        public static UserResponse from(User user, String jwtToken){
-            return new UserResponse(
-                    new Response(user.getEmail(), jwtToken, user.getUsername(), user.getBio(), user.getImage())
-            );
-        }
+    public String getPassword() {
+      return request.password;
     }
 
-    public static class UserLoginRequest{
-
-        @JsonProperty(value="user")
-        public Request request;
-
-        public String getEmail(){
-            return request.email;
-        }
-
-        public String getPassword(){
-            return request.password;
-        }
-
-
-        public static class Request{
-            public final String email;
-
-            public final String password;
-
-            public Request(String email, String password){
-                this.email = email;
-                this.password = password;
-            }
-
-        }
+    public String getImage() {
+      return request.image;
     }
+
+    public String getBio() {
+      return request.bio;
+    }
+
+    public static class Request {
+
+      public final String email;
+
+      public final String username;
+
+      public final String password;
+
+      public final String image;
+
+      public final String bio;
+
+      public Request(String email, String username, String password, String image, String bio) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.image = image;
+        this.bio = bio;
+      }
+
+    }
+  }
+
+  public static class UserCreateRequest {
+
+    @JsonProperty(value = "user")
+    public UserCreateRequest.Request request;
+
+    public User toUser() {
+      return request.toUser();
+    }
+
+    public static class Request {
+
+      private final String username;
+
+      private final String email;
+
+      private final String password;
+
+      public Request(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+      }
+
+      public User toUser() {
+        return new User(email, password, username, "", "");
+      }
+    }
+  }
+
+  public static class UserResponse {
+
+    @JsonProperty(value = "user")
+    public Response response;
+
+    public Response getResponse() {
+      return response;
+    }
+
+    public static class Response {
+
+      private final String email;
+
+      private final String token;
+
+      private final String username;
+
+      private final String bio;
+
+      private final String image;
+
+      private Response(String email, String token, String username, String bio, String image) {
+        this.email = email;
+        this.token = token;
+        this.username = username;
+        this.bio = bio;
+        this.image = image;
+      }
+
+      public String getEmail() {
+        return email;
+      }
+
+      public String getToken() {
+        return token;
+      }
+
+      public String getUsername() {
+        return username;
+      }
+
+      public String getBio() {
+        return bio;
+      }
+
+      public String getImage() {
+        return image;
+      }
+    }
+
+    public UserResponse(Response response) {
+      this.response = response;
+    }
+
+    public static UserResponse from(User user, String jwtToken) {
+      return new UserResponse(
+          new Response(user.getEmail(), jwtToken, user.getUsername(), user.getBio(),
+              user.getImage())
+      );
+    }
+  }
+
+  @NoArgsConstructor(access = AccessLevel.PROTECTED)
+  public static class UserLoginRequest {
+
+    @JsonProperty(value = "user")
+    public Request request;
+
+    public String getEmail() {
+      return request.email;
+    }
+
+    public String getPassword() {
+      return request.password;
+    }
+
+
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class Request {
+
+      public String email;
+
+      public String password;
+
+      public Request(String email, String password) {
+        this.email = email;
+        this.password = password;
+      }
+
+    }
+  }
 }
--- realworld/src/main/java/org/realworld/demo/domain/user/service/UserService.java
+++ realworld/src/main/java/org/realworld/demo/domain/user/service/UserService.java
@@ -1,49 +1,52 @@
 package org.realworld.demo.domain.user.service;
 
+import java.util.Optional;
 import org.realworld.demo.domain.user.entity.User;
 import org.realworld.demo.domain.user.repository.UserRepository;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class UserService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+  private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public User saveUser(User user) {
+    return userRepository.save(user);
+  }
+
+  public User updateUser(User originUser, String email, String username, String password,
+      String image, String bio) {
+    User updatedUser = originUser.update(email, password, username, bio, image);
+    return userRepository.save(updatedUser);
+  }
+
+  public User getUserByUsername(String username) {
+    return userRepository.findByUsername(username).orElseThrow();
+  }
+
+  public User getById(Long id) {
+    return userRepository.findById(id).orElseThrow();
+  }
+
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    return userRepository.findByEmail(email).orElseThrow();
+  }
+
+  public User login(String email, String password) {
+    Optional<User> maybeUser = userRepository.findByEmail(email);
+    User user = maybeUser.orElseThrow();
+    if (!user.getPassword().equals(password)) {
+      throw new IllegalArgumentException();
     }
-
-    public User saveUser(User user){
-        return userRepository.save(user);
-    }
-
-    public User updateUser(User originUser, String email, String username, String password, String image, String bio){
-        User updatedUser = originUser.update(email, password, username, bio, image);
-        return userRepository.save(updatedUser);
-    }
-
-    public User getUserByUsername(String username){
-        return userRepository.findByUsername(username).orElseThrow();
-    }
-
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email).orElseThrow();
-    }
-
-    public User login(String email, String password){
-        Optional<User> maybeUser = userRepository.findByEmail(email);
-        User user = maybeUser.orElseThrow();
-        if(!user.getPassword().equals(password)){
-            throw new BadCredentialsException("비밀번호가 맞지 않습니다");
-        }
-        return user;
-    }
+    return user;
+  }
 }
